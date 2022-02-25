770d9dfce4c620412425f9893012974b21bfabe8
andrewphm <andrpham@gmail.com>
2022-02-25 04:16:51 -0500

    Wrap app around a provider. Add persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Redux
+import { store, persistor } from '../redux/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default MyApp
