5c009ba75b853d16c5cc9b8c531d1816765a4443
andrewphm <andrpham@gmail.com>
2022-03-01 18:30:34 -0500

    Create profile menu and onClick handler

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,8 @@
 import {
+  AccountCircle,
   AddBox,
   AddBoxOutlined,
+  BookmarkBorder,
   Cancel,
   Explore,
   ExploreOutlined,
@@ -11,8 +13,9 @@ import {
   Search,
   Send,
   SendOutlined,
+  SettingsOutlined,
 } from '@mui/icons-material'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import logo from '../../../public/images/logo.png'
 import nopfp from '../../../public/images/nopfp.jpeg'
 import { useSelector } from 'react-redux'
@@ -23,6 +26,14 @@ const Header = () => {
   const [tab, setTab] = useState('home')
   const [inputIsActive, setInputIsActive] = useState(false)
   const [searchInput, setSearchInput] = useState('')
+  const menuRef = useRef(null)
+
+  const handleProfileClick = () => {
+    setTab((prev) => 'user')
+
+    menuRef.current.classList.toggle('opacity-0')
+    menuRef.current.classList.toggle('transform-none')
+  }
 
   const handleSearchClose = () => {
     setSearchInput((prev) => '')
@@ -31,7 +42,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="sticky top-0 flex h-[60px] w-screen items-center justify-center border bg-white px-3">
+      <header className="sticky top-0 flex h-[60px] w-screen items-center justify-center border bg-white px-5">
         <div className="flex w-full max-w-6xl items-center justify-between gap-x-2">
           {/* Logo */}
           <div className="relative flex h-[35px] w-[113px] min-w-[113px] cursor-pointer">
@@ -69,7 +80,7 @@ const Header = () => {
           </div>
 
           {/* Nav */}
-          <div className="flex items-center gap-x-4">
+          <div className="relative flex items-center gap-x-4">
             <div
               className="cursor-pointer "
               onClick={() => setTab((prev) => 'home')}
@@ -126,7 +137,7 @@ const Header = () => {
             </div>
 
             <div
-              onClick={() => setTab((prev) => 'user')}
+              onClickCapture={handleProfileClick}
               className={`flex cursor-pointer items-center justify-center rounded-full ${
                 tab === 'user' ? 'border border-black p-[1px]' : ''
               }`}
@@ -139,6 +150,31 @@ const Header = () => {
                   objectFit="contain"
                 />
               </div>
+            </div>
+
+            <div
+              ref={menuRef}
+              className="absolute top-12 -right-2 z-10 flex w-56 -translate-y-96  rounded-md bg-white opacity-0 shadow-[0px_0px_5px_1px_rgba(0,0,0,0.0975)] transition-opacity duration-500 ease-linear"
+            >
+              <ul className="h-full w-full text-sm">
+                <li className="flex cursor-pointer items-center gap-x-2 rounded-t-md py-[10px] px-4 hover:bg-gray-100">
+                  <AccountCircle fontSize="small" />
+                  <p>Profile</p>
+                </li>
+                <li className="flex cursor-pointer items-center gap-x-2 py-[10px] px-4 hover:bg-gray-100">
+                  <BookmarkBorder fontSize="small" />
+                  <p>Saved</p>
+                </li>
+                <li className="flex cursor-pointer items-center gap-x-2 py-[10px] px-4 hover:bg-gray-100">
+                  <SettingsOutlined fontSize="small" />
+                  <p>Settings</p>
+                </li>
+                <li className="flex cursor-pointer items-center gap-x-2 rounded-b-md border-t py-[10px] px-5 hover:bg-gray-100">
+                  <p>Sign Out</p>
+                </li>
+              </ul>
+
+              <div className="absolute right-[17px] -top-[9px] -z-10 h-4 w-4 rotate-45 border-l border-t bg-white"></div>
             </div>
           </div>
         </div>
