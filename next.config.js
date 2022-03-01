44b9b6b68c86158134b3cbb4b8e2c87c4df2de9e
andrewphm <andrpham@gmail.com>
2022-03-01 15:55:31 -0500

    Add menu items tab and state

--- next.config.js
+++ next.config.js
@@ -5,5 +5,11 @@ module.exports = {
     MONGODB_URI:
       'mongodb+srv://admin:admin@cluster0.61vc6.mongodb.net/mainDB?retryWrites=true&w=majority',
     JWT_SEC: '12345',
+    images: {
+      domains: [
+        'scontent-sjc3-1.cdninstagram.com',
+        'scontent-sjc3-1.cdninstagram.com',
+      ],
+    },
   },
 }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,25 +10,103 @@ import {
   Send,
   SendOutlined,
 } from '@mui/icons-material'
+import { useState } from 'react'
+import logo from '../../../public/images/logo.png'
+import nopfp from '../../../public/images/nopfp.jpeg'
 import { useSelector } from 'react-redux'
+import Image from 'next/image'
 
 const Header = () => {
   const user = useSelector((state) => state.user.currentUser)
+  const [tab, setTab] = useState('home')
+  console.log(user)
 
   return (
-    <nav>
-      <div>header</div>
-      <Home />
-      <HomeOutlined />
-      <Send />
-      <SendOutlined />
-      <AddBox />
-      <AddBoxOutlined />
-      <Explore />
-      <ExploreOutlined />
-      <Favorite />
-      <FavoriteBorder />
-    </nav>
+    <>
+      <header className="flex h-[60px] w-screen items-center justify-center border">
+        <div className="flex max-w-4xl items-center">
+          {/* Logo */}
+          <div className="relative h-auto w-32">
+            <Image src={logo} objectFit="contain" />
+          </div>
+
+          {/* Search Bar */}
+          <div>Search</div>
+
+          {/* Nav */}
+          <div className="flex items-center gap-x-4">
+            <div
+              className="cursor-pointer "
+              onClick={() => setTab((prev) => 'home')}
+            >
+              {tab === 'home' ? (
+                <Home sx={{ fontSize: 28 }} />
+              ) : (
+                <HomeOutlined sx={{ fontSize: 28 }} />
+              )}
+            </div>
+
+            <div
+              className="cursor-pointer "
+              onClick={() => setTab((prev) => 'send')}
+            >
+              {tab === 'send' ? (
+                <Send sx={{ fontSize: 28 }} />
+              ) : (
+                <SendOutlined sx={{ fontSize: 28 }} />
+              )}
+            </div>
+
+            <div
+              className="cursor-pointer "
+              onClick={() => setTab((prev) => 'add')}
+            >
+              {tab === 'add' ? (
+                <AddBox sx={{ fontSize: 28 }} />
+              ) : (
+                <AddBoxOutlined sx={{ fontSize: 28 }} />
+              )}
+            </div>
+
+            <div
+              className="cursor-pointer "
+              onClick={() => setTab((prev) => 'explore')}
+            >
+              {tab === 'explore' ? (
+                <Explore sx={{ fontSize: 30 }} />
+              ) : (
+                <ExploreOutlined sx={{ fontSize: 30 }} />
+              )}
+            </div>
+
+            <div
+              className="cursor-pointer "
+              onClick={() => setTab((prev) => 'favorite')}
+            >
+              {tab === 'favorite' ? (
+                <Favorite sx={{ fontSize: 28 }} />
+              ) : (
+                <FavoriteBorder sx={{ fontSize: 28 }} />
+              )}
+            </div>
+
+            <div
+              onClick={() => setTab((prev) => 'user')}
+              className={`flex cursor-pointer items-center justify-center rounded-full ${
+                tab === 'user' ? 'border border-black p-[1px]' : ''
+              }`}
+            >
+              <Image
+                src={nopfp}
+                height="30"
+                width="30"
+                className="rounded-full"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
