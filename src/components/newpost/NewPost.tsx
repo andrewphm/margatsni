c9abb2099eefda29fcfeab7ae007d9f2ec623bbe
andrewphm <andrpham@gmail.com>
2022-03-07 15:51:16 -0500

    Create Image preview jsx

--- src/components/newpost/NewPost.tsx
+++ src/components/newpost/NewPost.tsx
@@ -1,8 +1,23 @@
-import { Close } from '@mui/icons-material'
+import { Close, KeyboardBackspace } from '@mui/icons-material'
+import { useState } from 'react'
 
 const NewPost = ({ setShowNewPost }) => {
-  const handleCloseNewPost = (e) => {
-    if (e.target.id === 'overlay') setShowNewPost((prev) => false)
+  const [fileURL, setFileUrl] = useState(null)
+
+  const handleUserInputChange = (event) => {
+    //Take the file, upload to firebase.
+
+    setFileUrl((prev) => URL.createObjectURL(event.target.files[0]))
+
+    console.log(fileURL)
+  }
+
+  const handleCloseNewPost = (event) => {
+    if (event.target.id === 'overlay') setShowNewPost((prev) => false)
+  }
+
+  const handlePrevious = () => {
+    setFileUrl((prev) => null)
   }
 
   return (
@@ -11,15 +26,97 @@ const NewPost = ({ setShowNewPost }) => {
       className="absolute flex h-full w-full items-center justify-center bg-black bg-opacity-70"
       onClick={handleCloseNewPost}
     >
-      <div className="flex h-1/2 w-11/12 items-center justify-center bg-white">
-        <button className="bg-blue-btn text-white">Add photo</button>
-      </div>
+      <div className="relative flex min-h-[400px] w-11/12 max-w-xl flex-col items-center rounded-xl bg-white">
+        <div className="w-full border-b-2 py-3 text-center">
+          <h3 className="text-xl font-semibold">Create new post</h3>
+        </div>
 
-      <div
-        className="absolute right-10 top-10 cursor-pointer text-white"
-        onClick={() => setShowNewPost((prev) => false)}
-      >
-        <Close fontSize="large" />
+        {fileURL && (
+          <div className="my-3 flex h-full w-full flex-col items-center px-3">
+            <div className="w-full border">
+              <div className="flex max-h-[500px] w-full justify-center overflow-hidden ">
+                <img src={fileURL} alt="" className="object-contain" />
+              </div>
+
+              <div className="flex w-full">
+                <div className="w-full border p-3">
+                  <textarea
+                    name=""
+                    id=""
+                    rows="3"
+                    placeholder="Write a caption..."
+                    autoFocus
+                    className="w-full resize-none focus:outline-none"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {!fileURL && (
+          <div className="my-10 flex h-full w-full flex-col items-center gap-y-6">
+            <div className="">
+              <div className="mx-auto">
+                <svg
+                  aria-label="Icon to represent media such as images or videos"
+                  color="#262626"
+                  fill="#262626"
+                  height="112"
+                  role="img"
+                  viewBox="0 0 97.6 77.3"
+                  width="134"
+                >
+                  <path
+                    d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z"
+                    fill="currentColor"
+                  ></path>
+                  <path
+                    d="M84.7 18.4L58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5l-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z"
+                    fill="currentColor"
+                  ></path>
+                  <path
+                    d="M78.2 41.6L61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6l-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <h3></h3>
+            </div>
+
+            <label
+              htmlFor="user-image"
+              className="text-md cursor-pointer rounded-lg bg-blue-btn py-2 px-3 font-semibold text-white shadow-md transition hover:scale-[1.03] "
+            >
+              Select from device
+              <input
+                type="file"
+                name="user-image"
+                id="user-image"
+                accept="image/*"
+                className="hidden"
+                onChange={handleUserInputChange}
+              />
+            </label>
+          </div>
+        )}
+
+        <div
+          className="absolute right-3 top-3 cursor-pointer text-black"
+          onClick={() => setShowNewPost((prev) => false)}
+        >
+          <Close />
+        </div>
+
+        {fileURL && (
+          <div
+            className="absolute left-5 top-3 cursor-pointer text-black"
+            onClick={handlePrevious}
+          >
+            <KeyboardBackspace />
+          </div>
+        )}
       </div>
     </section>
   )
