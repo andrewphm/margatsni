caf8c33a2fcb956e1fa41d7096984d2ed9d737bc
andrewphm <andrpham@gmail.com>
2022-03-09 14:14:41 -0500

    Add dynamic next/link to user profile

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -359,47 +359,51 @@ const Header = ({ currentTab }) => {
                 className="absolute top-[50px] -right-3 z-10 flex w-56 -translate-y-96  rounded-md bg-white opacity-0 shadow-[0px_0px_5px_1px_rgba(0,0,0,0.0975)] transition-opacity duration-500 ease-linear"
               >
                 <ul className="h-full w-full text-sm">
-                  <li className="flex cursor-pointer items-center gap-x-2 rounded-t-md py-[10px] px-4 hover:bg-gray-100">
-                    <svg
-                      aria-label="Profile"
-                      color="#262626"
-                      fill="#262626"
-                      height="16"
-                      role="img"
-                      viewBox="0 0 24 24"
-                      width="16"
-                    >
-                      <circle
-                        cx="12.004"
-                        cy="12.004"
-                        fill="none"
-                        r="10.5"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        strokeWidth="2"
-                      ></circle>
-                      <path
-                        d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        strokeWidth="2"
-                      ></path>
-                      <circle
-                        cx="12.006"
-                        cy="9.718"
-                        fill="none"
-                        r="4.109"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        strokeWidth="2"
-                      ></circle>
-                    </svg>
-                    <p>Profile</p>
-                  </li>
+                  <Link href={`/${user.username}`}>
+                    <a>
+                      <li className="flex cursor-pointer items-center gap-x-2 rounded-t-md py-[10px] px-4 hover:bg-gray-100">
+                        <svg
+                          aria-label="Profile"
+                          color="#262626"
+                          fill="#262626"
+                          height="16"
+                          role="img"
+                          viewBox="0 0 24 24"
+                          width="16"
+                        >
+                          <circle
+                            cx="12.004"
+                            cy="12.004"
+                            fill="none"
+                            r="10.5"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            strokeWidth="2"
+                          ></circle>
+                          <path
+                            d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            strokeWidth="2"
+                          ></path>
+                          <circle
+                            cx="12.006"
+                            cy="9.718"
+                            fill="none"
+                            r="4.109"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            strokeWidth="2"
+                          ></circle>
+                        </svg>
+                        <p>Profile</p>
+                      </li>
+                    </a>
+                  </Link>
                   <li className="flex cursor-pointer items-center gap-x-2 py-[10px] px-4 hover:bg-gray-100">
                     <svg
                       aria-label="Saved"
@@ -532,47 +536,51 @@ const Header = ({ currentTab }) => {
             className="absolute top-[64px] right-[13px] z-10 flex w-56 -translate-y-96 rounded-md bg-white opacity-0 shadow-[0px_0px_5px_1px_rgba(0,0,0,0.0975)] transition-opacity duration-500 ease-linear sm:right-[20px]"
           >
             <ul className="h-full w-full text-sm">
-              <li className="flex cursor-pointer items-center gap-x-2 rounded-t-md py-[10px] px-4 hover:bg-gray-100">
-                <svg
-                  aria-label="Profile"
-                  color="#262626"
-                  fill="#262626"
-                  height="16"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  width="16"
-                >
-                  <circle
-                    cx="12.004"
-                    cy="12.004"
-                    fill="none"
-                    r="10.5"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    strokeWidth="2"
-                  ></circle>
-                  <path
-                    d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    strokeWidth="2"
-                  ></path>
-                  <circle
-                    cx="12.006"
-                    cy="9.718"
-                    fill="none"
-                    r="4.109"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    strokeWidth="2"
-                  ></circle>
-                </svg>
-                <p>Profile</p>
-              </li>
+              <Link href={`/${user.username}`}>
+                <a>
+                  <li className="flex cursor-pointer items-center gap-x-2 rounded-t-md py-[10px] px-4 hover:bg-gray-100">
+                    <svg
+                      aria-label="Profile"
+                      color="#262626"
+                      fill="#262626"
+                      height="16"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      width="16"
+                    >
+                      <circle
+                        cx="12.004"
+                        cy="12.004"
+                        fill="none"
+                        r="10.5"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        strokeWidth="2"
+                      ></circle>
+                      <path
+                        d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        strokeWidth="2"
+                      ></path>
+                      <circle
+                        cx="12.006"
+                        cy="9.718"
+                        fill="none"
+                        r="4.109"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        strokeWidth="2"
+                      ></circle>
+                    </svg>
+                    <p>Profile</p>
+                  </li>
+                </a>
+              </Link>
               <li className="flex cursor-pointer items-center gap-x-2 py-[10px] px-4 hover:bg-gray-100">
                 <svg
                   aria-label="Saved"
