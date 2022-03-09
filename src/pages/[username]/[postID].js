e14fb38e9f7c6bec69fcf4e6d5c8a46a7ac7d983
andrewphm <andrpham@gmail.com>
2022-03-09 17:54:18 -0500

    Create SSR prefetching posts

--- src/pages/[username]/[postID].js
+++ src/pages/[username]/[postID].js
@@ -1,9 +1,38 @@
-import React from 'react'
+import connectToDb from '../../lib/connectToDb'
+import Post from '../../models/Post'
+import Layout from '../../components/layouts/Layout'
 
 const UserPost = () => {
-  return <div>hello</div>
+  return (
+    <Layout>
+      <div>hihihi</div>
+    </Layout>
+  )
 }
 
 export default UserPost
 
-export async function getServerSideProps(context) {}
+export async function getServerSideProps(context) {
+  await connectToDb()
+
+  let postID = context.query.postID
+  let userQuery = context.query.username
+
+  let userPosts = await Post.findOne({ username: userQuery })
+
+  // Serialize results
+  userPosts = JSON.parse(JSON.stringify(userPosts))
+
+  console.log(userPosts)
+  console.log('Post id: ', postID)
+  console.log('Username: ', userQuery)
+
+  // 1. queryDB for users posts
+  // 2. query posts matching postID
+
+  return {
+    props: {
+      true: true,
+    },
+  }
+}
