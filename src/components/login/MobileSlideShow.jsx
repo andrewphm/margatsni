c35c3e0e80de0a768a80903b1c477b51784854bc
andrewphm <andrpham@gmail.com>
2022-03-15 13:19:55 -0400

    Change slideshow images

--- src/components/login/MobileSlideShow.jsx
+++ src/components/login/MobileSlideShow.jsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
-
-import phone from '../../../public/images/login/phone.png'
-import ss from '../../../public/images/login/ss.jpg'
-import ss2 from '../../../public/images/login/ss2.jpg'
-import ss3 from '../../../public/images/login/ss3.jpg'
-import ss4 from '../../../public/images/login/ss4.jpg'
-import ss5 from '../../../public/images/login/ss5.jpg'
+import { useEffect } from 'react'
+import iphone from '../../../public/images/login/iphone.png'
+import iphone1 from '../../../public/images/login/iphone1.png'
+import iphone2 from '../../../public/images/login/iphone2.png'
+import iphone3 from '../../../public/images/login/iphone3.png'
+import iphone4 from '../../../public/images/login/iphone4.png'
 
 const MobileSlideShow = () => {
   // Logic for slide show
@@ -28,10 +26,10 @@ const MobileSlideShow = () => {
         .getElementById(`slide${slide}`)
         ?.classList.remove('animate-[fade_10s_ease-in]', 'z-50')
 
-      if (slide === 4) {
+      if (slide === 3) {
         document.getElementById(`slide1`)?.classList.remove('opacity-0')
         document
-          .getElementById(`slide5`)
+          .getElementById(`slide4`)
           ?.classList.add('animate-[fade_10s_ease-in]', 'z-50')
       } else if (slide === 5) {
         document.getElementById(`slide2`)?.classList.remove('opacity-0')
@@ -48,7 +46,7 @@ const MobileSlideShow = () => {
       }
 
       slide += 1
-      if (slide === 6) slide = 1
+      if (slide === 5) slide = 1
     }, 7000)
 
     return () => {
@@ -59,70 +57,57 @@ const MobileSlideShow = () => {
   return (
     <div className={`relative hidden w-[440px] lg1:flex`}>
       <div className="relative w-full">
-        <Image draggable={false} src={phone} className="h-auto w-full" />
+        <Image draggable={false} src={iphone} className="h-auto w-full" />
       </div>
 
       <div
         id="slide1"
-        className="visible absolute left-[146px] top-[97px] z-50 opacity-0"
+        className="visible absolute left-[149px] top-[27px] z-50 opacity-0"
       >
         <Image
           draggable={false}
-          src={ss}
+          src={iphone1}
           className="h-auto w-[100px]"
-          height="413px"
+          height="510px"
           width="233px"
         />
       </div>
 
       <div
         id="slide2"
-        className=" visible absolute left-[146px] top-[97px] opacity-0"
+        className=" visible absolute left-[149px] top-[27px] opacity-0"
       >
         <Image
           draggable={false}
-          src={ss2}
+          src={iphone2}
           className="h-auto w-[100px]"
-          height="413px"
+          height="510px"
           width="233px"
         />
       </div>
 
       <div
         id="slide3"
-        className="visible absolute left-[146px] top-[97px] opacity-0 duration-[3000ms]"
+        className="visible absolute left-[149px] top-[27px] opacity-0 duration-[3000ms]"
       >
         <Image
           draggable={false}
-          src={ss3}
+          src={iphone3}
           className="h-auto w-[100px]"
-          height="413px"
+          height="510px"
           width="233px"
         />
       </div>
 
       <div
         id="slide4"
-        className="visible absolute left-[146px] top-[97px] opacity-0"
+        className="visible absolute left-[149px] top-[27px] opacity-0"
       >
         <Image
           draggable={false}
-          src={ss4}
+          src={iphone4}
           className="h-auto w-[100px]"
-          height="413px"
-          width="233px"
-        />
-      </div>
-
-      <div
-        id="slide5"
-        className=" visible absolute left-[146px] top-[97px] opacity-0"
-      >
-        <Image
-          draggable={false}
-          src={ss5}
-          className="h-auto w-[100px]"
-          height="413px"
+          height="510px"
           width="233px"
         />
       </div>
