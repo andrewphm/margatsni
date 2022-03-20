c7de45f8898d21c94a4b9ca0ff05a151602d0b97
andrewphm <andrpham@gmail.com>
2022-03-20 15:02:16 -0400

    SSR testing

--- src/pages/p/[username]/index.js
+++ src/pages/p/[username]/index.js
@@ -7,30 +7,30 @@ import ProfileContent from '../../../components/profile/ProfileContent'
 import Post from '../../../models/Post'
 
 export default function Test({ userData }) {
-  // If user cannot be found.
-  if (!userData) {
-    return (
-      <Layout>
-        <div className="my-10 mx-auto flex flex-col gap-y-5 px-10 text-center">
-          <p className="text-2xl font-semibold">
-            Sorry, this page isn't available.
-          </p>
+  // // If user cannot be found.
+  // if (!userData) {
+  //   return (
+  //     <Layout>
+  //       <div className="my-10 mx-auto flex flex-col gap-y-5 px-10 text-center">
+  //         <p className="text-2xl font-semibold">
+  //           Sorry, this page isn't available.
+  //         </p>
 
-          <p>
-            The link you followed may be broken, or the page may have been
-            removed.{' '}
-            <Link href="/">
-              <a>
-                <span className="cursor-pointer text-blue-btn">
-                  Click here to go back home.
-                </span>
-              </a>
-            </Link>
-          </p>
-        </div>
-      </Layout>
-    )
-  }
+  //         <p>
+  //           The link you followed may be broken, or the page may have been
+  //           removed.{' '}
+  //           <Link href="/">
+  //             <a>
+  //               <span className="cursor-pointer text-blue-btn">
+  //                 Click here to go back home.
+  //               </span>
+  //             </a>
+  //           </Link>
+  //         </p>
+  //       </div>
+  //     </Layout>
+  //   )
+  // }
 
   return (
     <Layout>
